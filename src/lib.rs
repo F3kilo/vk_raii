060e8e723b3185627c1ed4c0c54c5e644afbc24e
KirFed <f3lkilo@yandex.ru>
2020-11-27 09:44:23 +0300

    Traits and generics.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,78 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::sync::Arc;
+
+pub trait RawHandle {
+    type Dependencies;
+
+    fn destroy(&self, info: &Self::Dependencies);
+}
+
+pub struct UniqueHandle<T, D>
+where
+    T: RawHandle<Dependencies = D>,
+{
+    handle: T,
+    dependencies: D,
+}
+
+impl<T, D> UniqueHandle<T, D>
+where
+    T: RawHandle<Dependencies = D>,
+{
+    /// # Safety
+    /// * `handle` must be valud initialized handle;
+    /// * `dependencies` must contain valid and initialized handles;
+    pub unsafe fn new(handle: T, dependencies: D) -> Self {
+        Self {
+            handle,
+            dependencies,
+        }
+    }
+
+    pub fn handle(&self) -> &T {
+        &self.handle
+    }
+
+    pub fn dependencies(&self) -> &D {
+        &self.dependencies
+    }
+}
+
+impl<T, D> Drop for UniqueHandle<T, D>
+where
+    T: RawHandle<Dependencies = D>,
+{
+    fn drop(&mut self) {
+        self.handle.destroy(&self.dependencies)
+    }
+}
+
+#[derive(Clone)]
+struct Handle<T, D>
+where
+    T: RawHandle<Dependencies = D>,
+{
+    handle: Arc<UniqueHandle<T, D>>,
+}
+
+impl<T, D> Handle<T, D>
+where
+    T: RawHandle<Dependencies = D>,
+{
+    /// # Safety
+    /// * `handle` must be valud initialized handle;
+    /// * `dependencies` must contain valid and initialized handles;
+    pub unsafe fn new(handle: T, destroy_info: D) -> Self {
+        let unique = UniqueHandle::new(handle, destroy_info);
+        Self {
+            handle: Arc::new(unique),
+        }
+    }
+
+    pub fn handle(&self) -> &T {
+        &self.handle.handle()
+    }
+
+    pub fn dependencies(&self) -> &D {
+        &self.handle.dependencies()
     }
 }
